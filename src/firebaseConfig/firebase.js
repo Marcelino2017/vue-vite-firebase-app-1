fb93cf6c86e5fd20f17a68b591bc3dfeff342069
Marcelino <marcelinomebe@gmail.com>
2024-03-07 19:30:06 -0500

    Configuracion de firebase 10

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXLSgxzsjuA2vjlj9f6tUMKY2XL_rmvio",
+  authDomain: "vur-3-2024.firebaseapp.com",
+  projectId: "vur-3-2024",
+  storageBucket: "vur-3-2024.appspot.com",
+  messagingSenderId: "865404755877",
+  appId: "1:865404755877:web:1743784b99655949f0d24c",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+
 createApp(App).use(router).use(createPinia()).mount('#app');
