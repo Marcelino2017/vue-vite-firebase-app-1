24c72db77d317b7508223a7a8fdd59dd9ee6278b
Marcelino <marcelinomebe@gmail.com>
2024-03-08 17:43:55 -0500

    Se realizo el inicio de sesion

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,35 @@
 <script setup>
+import { ref } from "vue";
+import { useUserStore } from '../stores/user.js'
+//import { useRouter } from 'vue-router'
 
+const userStore = useUserStore();
+//const router = useRouter()
+
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = async () => {
+  console.log(password.value.length);
+  console.log(email.value);
+  console.log(password.value);
+ /*  if (email.value|| password.value) {
+    return alert("Por favor, complete todos los campos");
+  } */
+  
+  console.log("Form submitted");
+  await userStore.loginUser(email.value, password.value);
+  //router.push('/')
+};
 </script>
 
 <template>
+  <div>
     <h1>Login</h1>
+    <form @submit.prevent="handleSubmit">
+      <input type="email" v-model.trim="email" placeholder="Ingrese email" />
+      <input type="password" v-model.trim="password" placeholder="Ingrese password" />
+      <button type="submit">Acceso</button>
+    </form>
+  </div>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
