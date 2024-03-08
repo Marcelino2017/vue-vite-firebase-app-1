bf0a1bc01c594b68207395c1e798602976e4ec3c
Marcelino <marcelinomebe@gmail.com>
2024-03-08 17:46:23 -0500

    Se crear los metodos en accion para el login, registro y cierre de sesion

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword} from "firebase/auth";
 import { auth } from "../firebaseConfig/firebase";
+import router from "../router";
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
@@ -17,6 +18,27 @@ export const useUserStore = defineStore('userStore', {
                 );
                 console.log(user);
                 this.userData = { email: user.email, uid: user.uid }
+                router.push('/')
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async loginUser(email, password) {
+            try {
+                const { user } = await signInWithEmailAndPassword(auth, email, password)
+                console.log(user);
+                this.userData = { email: user.email, uid: user.uid }
+                router.push('/')
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async logoutUser() {
+            try {
+                await signOut(auth);
+                this.userData = null;
+                router.push('/login')
+                console.log('Usuario deslogueado');
             } catch (error) {
                 console.log(error);
             }
